7d7fc1044b291cd649ae13f039c09b49468dfed8
NathanD0wd <122576040+NathanD0wd@users.noreply.github.com>
2023-02-27 15:07:10 -0800

    Create Lab3Part1.py

---
+++ Lab3Part1.py
@@ -0,0 +1,13 @@
+import board
+from adafruit_hcsr04 import HCSR04
+from time import sleep
+# Setup Section
+sonar = HCSR04(trigger_pin=board.TX, echo_pin=board.A6)
+# Function Section
+# Loop Section
+while True:
+  try:
+    print((sonar.distance,))
+  except RuntimeError:
+    print("Retrying!")
+  sleep(0.5
